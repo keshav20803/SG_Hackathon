68bbc94ac81c1986c9daf1428d252e97d098edef
Akshat Jain <93882347+akshatjainei@users.noreply.github.com>
2023-08-30 22:27:35 +0530

    Replaced connectionString with env string

--- db/connect.js
+++ db/connect.js
@@ -1,7 +1,5 @@
 const mongoose = require('mongoose')
 
-const connectionString = 'mongodb+srv://jainakshat447:node123@taskmanager.xrlpzeb.mongodb.net/?retryWrites=true&w=majority'
-
 const connectDB = (url) =>{
     return mongoose
     .connect(url,{
